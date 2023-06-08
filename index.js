f16e7dfe88b73c573a5e6e1965da2dae2c9cd440
ZhivkoAndreev <zhia@nuuday.dk>
2023-06-08 23:11:22 +0200

    a

--- index.js
+++ index.js
@@ -6,61 +6,48 @@ const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-server.use((req, res, next) => {
-  if (req.method === "POST" && req.originalUrl === "/patients") {
-    // Generate a unique ID for the new patient
-    req.body.id = Date.now().toString();
-  }
-  next();
+
+// Custom routes for individual rooms and patients
+server.get("/urology/room/:roomId", (req, res) => {
+  const roomId = req.params.roomId;
+  const room = router.db.get("urology.room").find({ id: roomId }).value();
+  res.json(room);
 });
 
-// Custom route for getting a specific patient
-server.get("/patients/:patientId", (req, res) => {
-  const patientId = req.params.patientId;
-  const patients = router.db
-    .get("urology")
-    .flatMap((urology) => urology.patients)
-    .value();
-  const patient = patients.find((patient) => patient.id === patientId);
+server.post("/urology/room/:roomId/patient", (req, res) => {
+  const roomId = req.params.roomId;
+  const newPatient = req.body;
+  const room = router.db.get("urology.room").find({ id: roomId }).value();
+  room.patients.push(newPatient);
+  router.db.write();
+  res.status(201).json(newPatient);
+});
 
+server.get("/urology/room/:roomId/patient/:patientId", (req, res) => {
+  const roomId = req.params.roomId;
+  const patientId = req.params.patientId;
+  const room = router.db.get("urology.room").find({ id: roomId }).value();
+  const patient = room.patients.find((patient) => patient.id === patientId);
   if (patient) {
     res.json(patient);
   } else {
-    res.status(404).json({ error: "Patient not found" });
+    res.sendStatus(404);
   }
 });
 
-// Custom route for adding a new patient
-server.post("/patients", (req, res) => {
-  const patient = req.body;
-  router.db
-    .get("urology")
-    .find({ id: patient.urologyId })
-    .get("patients")
-    .push(patient)
-    .write();
-
-  res.status(201).json(patient);
-});
-
-// Custom route for deleting a patient
-server.delete("/patients/:patientId", (req, res) => {
+server.delete("/urology/room/:roomId/patient/:patientId", (req, res) => {
+  const roomId = req.params.roomId;
   const patientId = req.params.patientId;
-  const patients = router.db
-    .get("urology")
-    .flatMap((urology) => urology.patients)
-    .value();
-  const index = patients.findIndex((patient) => patient.id === patientId);
-
-  if (index !== -1) {
-    router.db
-      .get("urology")
-      .flatMap((urology) => urology.patients)
-      .splice(index, 1)
-      .write();
-    res.status(200).json({ message: "Patient deleted" });
+  const room = router.db.get("urology.room").find({ id: roomId }).value();
+  const patientIndex = room.patients.findIndex(
+    (patient) => patient.id === patientId
+  );
+  if (patientIndex !== -1) {
+    room.patients.splice(patientIndex, 1);
+    router.db.write();
+    res.sendStatus(204);
   } else {
-    res.status(404).json({ error: "Patient not found" });
+    res.sendStatus(404);
   }
 });
 
