cc58a9373fb176c8b154e62cd8bb189cf60fe49a
ZhivkoAndreev <zhia@nuuday.dk>
2023-06-08 20:29:26 +0200

    a

--- index.js
+++ index.js
@@ -8,55 +8,39 @@ server.use(middlewares);
 server.use(jsonServer.bodyParser); // Add this line to parse request body as JSON
 server.use(router);
 
-// Custom route handler for the delete endpoint
-server.delete("/urology/:urologyId/patients/:patientId", (req, res) => {
-  const { urologyId, patientId } = req.params;
-
-  // Find the urology entry in the JSON data
-  const urology = router.db
-    .get("urology")
-    .find({ id: parseInt(urologyId) })
-    .value();
-
-  if (urology) {
-    // Find the patient in the patients array
-    const updatedPatients = urology.patients.filter(
-      (patient) => patient.id !== parseInt(patientId)
-    );
-
-    // Update the patients array for the urology entry
-    urology.patients = updatedPatients;
-
-    // Return a success status
-    res.sendStatus(204);
-  } else {
-    // Return a not found status if urology not found
-    res.sendStatus(404);
-  }
-});
-
-server.put("/urology/:urologyId/patients/:patientId", (req, res) => {
+// Dynamic route handler for delete and put operations on patients
+server.use("/urology/:urologyId/patients/:patientId", (req, res) => {
   const { urologyId, patientId } = req.params;
   const updatedPatient = req.body;
 
   // Find the urology entry in the JSON data
   const urology = router.db
     .get("urology")
-    .find({ id: parseInt(urologyId) })
+    .find({ id: urologyId }) // Compare id as string
     .value();
 
   if (urology) {
     // Find the patient in the patients array
     const patient = urology.patients.find(
-      (patient) => patient.id === parseInt(patientId)
+      (patient) => patient.id === patientId // Compare id as string
     );
 
     if (patient) {
-      // Update the patient's data
-      Object.assign(patient, updatedPatient);
+      if (req.method === "DELETE") {
+        // Delete the patient from the patients array
+        urology.patients = urology.patients.filter(
+          (patient) => patient.id !== patientId // Compare id as string
+        );
 
-      // Return the updated patient
-      res.json(patient);
+        // Return a success status
+        res.sendStatus(204);
+      } else if (req.method === "PUT") {
+        // Update the patient's data
+        Object.assign(patient, updatedPatient);
+
+        // Return the updated patient
+        res.json(patient);
+      }
     } else {
       // Return a not found status if patient not found
       res.sendStatus(404);
